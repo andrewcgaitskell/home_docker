aca45f7e7765cf4af6a5efa1adc9b311ce9ab6f0
andrewcgaitskell <andrew@gaitskell.com>
2024-08-10 15:06:54 +0100

    Update A_create_all_images.sh

--- A_create_all_images.sh
+++ A_create_all_images.sh
@@ -2,16 +2,16 @@
 
 # remember to copy the .env file into create before running this
 
-cp ./.env ../mariadb/.env
+cp .env /home_podman/mariadb/.env
 ##cp ./.env ../fastapi_data/app/.env
 ##cp ./.env ../fastapi_about/app/.env
 ##cp ./.env ../application/app/.env
 ##cp ./.env ../application/app/baseapp/.env
 ##cp ./.env ../backup_db/.env
 
-## set the variables for the mariadb container
+## set the variables for the containers
 source setenv.sh
-source ./mariadb/createinitsql.sh
+source /home_podman/mariadb/createinitsql.sh
 
 ## mariadb image
 
@@ -20,9 +20,9 @@ cd /home_podman/mariadb
 rm -rf /home_data/mariadb
 mkdir /home_data/mariadb
 
-podman stop container_mariadb
-podman rm container_mariadb
-podman rmi mariadb_1
+podman stop mariadb_container
+podman rm mariadb_container
+podman rmi mariadb_image
 
 podman build \
 --build-arg=BUILD_ENV_UID=${ENV_UID} \
@@ -33,55 +33,69 @@ podman build \
 --build-arg=BUILD_ENV_MARIADB_PASSWORD=${ENV_MARIADB_PASSWORD} \
 --build-arg=BUILD_ENV_MARIADB_ROOT_PASSWORD=${ENV_MARIADB_ROOT_PASSWORD} \
 --build-arg=BUILD_ENV_MARIADB_DATABASE=${ENV_MARIADB_DATABASE} \
--t mariadb_1 .
+-t mariadb_image .
 
 ##-v /HOST-DIR:/CONTAINER-DIR
 
-## DastAPI Data Image
+## Jupyter
 
 cd /home_podman/jupyter_2
 
-rm -rf /opt/dmtools/code/dmtools/basecode/fastapi_data/app/migrations/
+podman stop jupyter_container
+podman rm jupyter_container
 
-podman stop container_jupyter_2
-podman rm container_jupyter_2
-
-podman rmi image_jupyter_2
+podman rmi jupyter_image
 
 podman build \
 --build-arg=BUILD_ENV_UID=${ENV_UID} \
 --build-arg=BUILD_ENV_USERNAME=${ENV_USERNAME} \
 --build-arg=BUILD_ENV_GID=${ENV_GID} \
 --build-arg=BUILD_ENV_GROUPNAME=${ENV_GROUPNAME} \
--f Dockerfile -t image_jupyter_2
-
+-f Dockerfile -t jupyter_image
 
 cd /home_podman/reactjs
 
-podman stop container_reactjs
-podman rm container_reactjs
+podman stop reactjs_container
+podman rm reactjs_container
 
-podman rmi image_reactjs
+podman rmi reactjs_image
 
 podman build \
 --build-arg=BUILD_ENV_UID=${ENV_UID} \
 --build-arg=BUILD_ENV_USERNAME=${ENV_USERNAME} \
 --build-arg=BUILD_ENV_GID=${ENV_GID} \
 --build-arg=BUILD_ENV_GROUPNAME=${ENV_GROUPNAME} \
--f Dockerfile -t image_reactjs
+-f Dockerfile -t reactjs_image
 
-## application image
+## mosquitto image
 
 cd /home_podman/mosquitto
 
-podman stop container_mosquitto
-podman rm container_mosquitto
+podman stop mosquitto_container
+podman rm mosquitto_container
 
-podman rmi image_mosquitto:latest
+podman rmi mosquitto_image:latest
 
 podman build \
 --build-arg=BUILD_ENV_UID=${ENV_UID} \
 --build-arg=BUILD_ENV_USERNAME=${ENV_USERNAME} \
 --build-arg=BUILD_ENV_GID=${ENV_GID} \
 --build-arg=BUILD_ENV_GROUPNAME=${ENV_GROUPNAME} \
--f Dockerfile -t image_mosquitto .
+-f Dockerfile -t mosquitto_image .
+
+## dash image
+
+cd /home_podman/dash
+
+podman stop dash_container
+podman rm dash_container
+
+podman rmi dash_image:latest
+
+podman build \
+--build-arg=BUILD_ENV_UID=${ENV_UID} \
+--build-arg=BUILD_ENV_USERNAME=${ENV_USERNAME} \
+--build-arg=BUILD_ENV_GID=${ENV_GID} \
+--build-arg=BUILD_ENV_GROUPNAME=${ENV_GROUPNAME} \
+-f Dockerfile -t dash_image .
+
