a1bacd137f8f10149774c632ab62575e489001f9
andrewcgaitskell <andrew@gaitskell.com>
2024-08-10 11:45:55 -0400

    Create clean+create.sh

---
+++ mariadb/clean+create.sh
@@ -0,0 +1,24 @@
+## mariadb image
+
+cd /home_podman/mariadb
+
+rm -rf /home_data/mariadb
+mkdir /home_data/mariadb
+
+podman stop mariadb_container
+podman rm mariadb_container
+podman rmi mariadb_image
+
+podman build \
+--build-arg=BUILD_ENV_UID=${ENV_UID} \
+--build-arg=BUILD_ENV_USERNAME=${ENV_USERNAME} \
+--build-arg=BUILD_ENV_GID=${ENV_GID} \
+--build-arg=BUILD_ENV_GROUPNAME=${ENV_GROUPNAME} \
+--build-arg=BUILD_ENV_MARIADB_USER=${ENV_MARIADB_USER} \
+--build-arg=BUILD_ENV_MARIADB_PASSWORD=${ENV_MARIADB_PASSWORD} \
+--build-arg=BUILD_ENV_MARIADB_ROOT_PASSWORD=${ENV_MARIADB_ROOT_PASSWORD} \
+--build-arg=BUILD_ENV_MARIADB_DATABASE=${ENV_MARIADB_DATABASE} \
+-t mariadb_image .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
