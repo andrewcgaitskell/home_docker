edf6d97cf7ddf670f11ba8ecb5b213e4a0d233e5
andrewcgaitskell <andrew@gaitskell.com>
2024-08-10 15:56:18 -0400

    Update createinitsql.sh

--- mariadb/createinitsql.sh
+++ mariadb/createinitsql.sh
@@ -1,7 +1,7 @@
-echo "CREATE USER '${MARIADB_USERNAME}'@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';" > /home_podman/mariadb/init.sql
+echo "CREATE USER '${ENV_MARIADB_USERNAME}'@'localhost' IDENTIFIED BY '${ENV_MARIADB_PASSWORD}';" > /home_podman/mariadb/init.sql
 
-echo "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USERNAME}'@'localhost' WITH GRANT OPTION;" >> /home_podman/mariadb/init.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '${ENV_MARIADB_USERNAME}'@'localhost' WITH GRANT OPTION;" >> /home_podman/mariadb/init.sql
 
-echo "ALTER USER '${MARIADB_USERNAME}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';" >> /home_podman/mariadb/init.sql
+echo "ALTER USER '${ENV_MARIADB_USERNAME}'@'%' IDENTIFIED BY '${ENV_MARIADB_PASSWORD}';" >> /home_podman/mariadb/init.sql
 
-echo "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USERNAME}'@'%' WITH GRANT OPTION;" >> /home_podman/mariadb/init.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '${ENV_MARIADB_USERNAME}'@'%' WITH GRANT OPTION;" >> /home_podman/mariadb/init.sql
