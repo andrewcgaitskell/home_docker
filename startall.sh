358646795b30b082ba70df3f045dbe9e4bde3426
andrewcgaitskell <andrew@gaitskell.com>
2024-08-14 09:05:53 -0400

    Update startall.sh

--- startall.sh
+++ startall.sh
@@ -1,7 +1,8 @@
+podman pod start home_pod
 podman start mariadb_container
 podman start jupyter_container
 podman start reactjs_container
 podman start mosquitto_container
 podman start dash_container
-podman pod start home_pod
+
 
