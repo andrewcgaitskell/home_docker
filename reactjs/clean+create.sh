b328606d8615c6a8d9631bd6777b2755579df431
andrewcgaitskell <andrew@gaitskell.com>
2024-08-14 08:13:18 -0400

    Update clean+create.sh

--- reactjs/clean+create.sh
+++ reactjs/clean+create.sh
@@ -1,4 +1,4 @@
-cd /home_podman/reactjs
+cd /home/home_user/Code/home_podman/reactjs
 
 podman stop reactjs_container
 podman rm reactjs_container
@@ -7,7 +7,7 @@ podman rmi reactjs_image_root
 podman rmi reactjs_image_user
 
 podman build \
--f Dockerfile_root -t reactjs_image_root
+-f Dockerfile_root -t reactjs_image
 
 #podman build \
 #--build-arg=BUILD_ENV_UID=${ENV_UID} \
