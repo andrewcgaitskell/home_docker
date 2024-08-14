86373a7baa635d1d70342e66c381021c13f68c83
andrewcgaitskell <andrew@gaitskell.com>
2024-08-14 09:04:58 -0400

    Create startall.sh

---
+++ startall.sh
@@ -0,0 +1,7 @@
+podman start mariadb_container
+podman start jupyter_container
+podman start reactjs_container
+podman start mosquitto_container
+podman start dash_container
+podman pod start home_pod
+
